4a7e30f00e4a1d6b5bfdff9738948f9de627b01b
JohnAndante <walker_contas@hotmail.com>
2025-10-23 18:58:55 -0300

    #12 - feat: Crated CPF and CNPJ validators

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -80,3 +80,80 @@ export const formatCpfCnpj = (value: string | null) => {
     // Se não for CPF nem CNPJ, retorna o original
     return value;
 };
+
+export const validateCPF = (cpf: string): boolean => {
+    let sum = 0
+    let remaining: number
+
+    const strCPF = String(cpf).replace(/[^\d]/g, '')
+
+    if (strCPF.length !== 11)
+        return false
+
+    if ([
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999',
+    ].indexOf(strCPF) !== -1)
+        return false
+
+    for (let i = 1; i <= 9; i++)
+        sum = sum + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+
+    remaining = (sum * 10) % 11
+
+    if ((remaining == 10) || (remaining == 11))
+        remaining = 0
+
+    if (remaining != parseInt(strCPF.substring(9, 10)))
+        return false
+
+    sum = 0
+
+    for (let i = 1; i <= 10; i++)
+        sum = sum + parseInt(strCPF.substring(i - 1, i)) * (12 - i)
+
+    remaining = (sum * 10) % 11
+
+    if ((remaining == 10) || (remaining == 11))
+        remaining = 0
+
+    if (remaining != parseInt(strCPF.substring(10, 11)))
+        return false
+
+    return true
+};
+
+export const validateCNPJ = (cnpj: string): boolean => {
+    const b = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+    const c = String(cnpj).replace(/[^\d]/g, '')
+
+    if (c.length !== 14)
+        return false
+
+    if (/0{14}/.test(c))
+        return false
+
+    let n = 0
+    for (let i = 0; i < 12; i++) {
+        n += Number(c[i]) * b[i + 1]
+    }
+    if (Number(c[12]) !== (((n %= 11) < 2) ? 0 : 11 - n))
+        return false
+
+    n = 0
+    for (let i = 0; i <= 12; i++) {
+        n += Number(c[i]) * b[i]
+    }
+    if (Number(c[13]) !== (((n %= 11) < 2) ? 0 : 11 - n))
+        return false
+
+    return true
+}
