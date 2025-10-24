805c157796a92a1525e73e41e9796d53b8afa2b8
Walker Silvestre <walker_contas@hotmail.com>
2025-10-24 01:19:39 -0300

    #12 - refactor: Renamed create user component

--- src/pages/UsuariosPage/components/index.ts
+++ src/pages/UsuariosPage/components/index.ts
@@ -3,7 +3,7 @@
 // ===========================
 
 export { UsersList } from './users-list';
-export { UserCreateForm } from './user-new-form';
+export { UserCreateForm } from './user-create-form';
 export { UserEditForm } from './user-edit-form';
 export { UserView } from './user-view';
 export { DeleteUserModal } from './delete-user-modal';
